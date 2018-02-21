94da4634f453eb6577ad707a553b75452d82b5dc
Brett Hemes <brhemes@mmm.com>
2018-02-21 15:31:10 -0600

    PR fix: copyright

--- kuka_eki_hw_interface/src/kuka_eki_hw_interface.cpp
+++ kuka_eki_hw_interface/src/kuka_eki_hw_interface.cpp
@@ -1,7 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- * Copyright (c) 2018, Brett Hemes (3M)
+ * Copyright (c) 2018, 3M
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
